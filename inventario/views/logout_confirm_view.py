1ef3f1c3791ee68ff589f21b8433515105172d90
FelipeNascimento-dev <felipe.nasci.dev@gmail.com>
2025-06-09 15:09:26 -0300

    login required

--- inventario/views/logout_confirm_view.py
+++ inventario/views/logout_confirm_view.py
@@ -1,5 +1,7 @@
 from django.contrib.auth import logout
 from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
 
+@login_required(login_url='inventario:login')
 def logout_confirm_view(request):
     return render(request, 'inventario/logout_confirm.html')
--- inventario/views/logout_view.py
+++ inventario/views/logout_view.py
@@ -1,6 +1,8 @@
 from django.contrib.auth import logout
 from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
 
+@login_required(login_url='inventario:login')
 def logout_view(request):
     logout(request)
     return redirect('inventario:login')
