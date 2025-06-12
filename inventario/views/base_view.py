1eda57bb81c5654bf840d6b5c97263442ac21076
FelipeNascimento-dev <felipe.nasci.dev@gmail.com>
2025-06-12 13:26:17 -0300

    feat: status lotes

--- inventario/views/base_view.py
+++ inventario/views/base_view.py
@@ -1,8 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from ..models import LoteBipagem
 from django.contrib.auth.decorators import login_required
 
 @login_required(login_url='inventario:login')
 def index(request):
+    if request.method == 'POST' and 'fechar_lote_id' in request.POST:
+        lote_id = request.POST.get('fechar_lote_id')
+        lote = get_object_or_404(LoteBipagem, id=lote_id)
+        lote.status = 'processando'  # ou 'processando', se preferir
+        lote.save()
+        return redirect('inventario:index')
+
     lotes = LoteBipagem.objects.all().order_by('-criado_em')
     return render(request, 'inventario/index.html', {'lotes': lotes})
