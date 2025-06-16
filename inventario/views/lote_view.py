ff70a61ce0f7f0a586cae98a77afd2cbd7a11382
FelipeNascimento-dev <felipe.nasci.dev@gmail.com>
2025-06-16 17:58:14 -0300

    feat: paginacao na bipagem

--- inventario/views/lote_view.py
+++ inventario/views/lote_view.py
@@ -2,10 +2,11 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.urls import reverse
 from ..models import LoteBipagem, Caixa
 from ..forms import CaixaForm
+from django.core.paginator import Paginator
 
 def lote(request, lote_id):
     lote = get_object_or_404(LoteBipagem, id=lote_id)
-    
+
     if request.method == 'POST':
         if 'encerrar_caixa' in request.POST:
             caixa_aberta = lote.caixas.filter(status='Iniciada').last()
@@ -13,7 +14,7 @@ def lote(request, lote_id):
                 caixa_aberta.status = 'Finalizada'
                 caixa_aberta.save()
             return redirect('inventario:lote', lote_id=lote.id)
-        
+
         form = CaixaForm(request.POST)
         if form.is_valid():
             caixa = form.save(commit=False)
@@ -24,13 +25,17 @@ def lote(request, lote_id):
     else:
         form = CaixaForm()
 
-    caixas = Caixa.objects.filter(lote=lote).order_by('-id')
+    caixas_list = Caixa.objects.filter(lote=lote).order_by('-id')
+    paginator = Paginator(caixas_list, 10) 
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+
     caixas_abertas = lote.caixas.filter(status='Iniciada').exists()
     lote_bloqueado = lote.status in ['fechado', 'cancelado']
 
     context = {
         'form': form,
-        'caixas': caixas,
+        'page_obj': page_obj,      
         'lote': lote,
         'caixas_abertas': caixas_abertas,
         'lote_bloqueado': lote_bloqueado
