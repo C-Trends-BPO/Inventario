8db0a0257d596c1cfa989722c3ea784ec785ba61
FelipeNascimento-dev <felipe.nasci.dev@gmail.com>
2025-06-17 15:43:27 -0300

    feat: nome da PA

--- inventario/views/criar_lote_view.py
+++ inventario/views/criar_lote_view.py
@@ -5,10 +5,18 @@ from django.contrib.auth.decorators import login_required
 @login_required(login_url='inventario:login')
 def criar_lote_view(request):
     if request.method == 'POST':
-        lote= LoteBipagem.objects.create(
-            user_created=request.user,
-            group_user=request.user.groups.first(),
-        )
-        lote_id  = lote.id
-        return redirect('inventario:lote', lote_id) 
+        grupo = request.user.groups.first()
+        if grupo:
+            nome_grupo = grupo.name
+            if nome_grupo.startswith("INV_PA_"):
+                nome_exibicao = nome_grupo.replace("INV_PA_", "", 1)
+            else:
+                nome_exibicao = nome_grupo
+
+            lote= LoteBipagem.objects.create(
+                user_created=request.user,
+                group_user=grupo,
+            )
+
+        return redirect('inventario:lote', lote.id) 
     return render(request, 'inventario/criar_lote.html')
