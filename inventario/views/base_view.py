feffdc54c3c3f52235f04e349c76a22b6c6c27b7
FelipeNascimento-dev <felipe.nasci.dev@gmail.com>
2025-06-17 09:18:31 -0300

    feat: search label

--- inventario/views/base_view.py
+++ inventario/views/base_view.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.core.paginator import Paginator
+from django.db.models import Q
 from ..models import LoteBipagem
 from django.contrib.auth.decorators import login_required
 
@@ -12,13 +13,42 @@ def index(request):
         lote.save()
         return redirect('inventario:index')
 
-    lotes_list = LoteBipagem.objects.all().order_by('-criado_em')
-    paginator = Paginator(lotes_list, 10) 
+    busca = request.GET.get('q', '')
 
+    lotes_list = LoteBipagem.objects.all().order_by('-criado_em')
+    if busca:
+        lotes_list = lotes_list.filter(
+            Q(id__icontains=busca) |
+            Q(status__icontains=busca) |  
+            Q(user_created__username__icontains=busca)
+        )
+
+    paginator = Paginator(lotes_list, 10)
     page_number = request.GET.get('page')
     page_obj = paginator.get_page(page_number)
 
     return render(request, 'inventario/index.html', {
         'lotes': page_obj.object_list,
         'page_obj': page_obj
+    })
+
+
+def listar_lotes(request):
+    busca = request.GET.get('q', '')
+    lotes = LoteBipagem.objects.all()
+
+    if busca:
+        lotes = lotes.filter(
+            Q(id__icontains=busca) |
+            Q(status__icontains=busca) |
+            Q(user_created__username__icontains=busca)
+        )
+
+    paginator = Paginator(lotes, 10)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+
+    return render(request, 'inventario/listar_lotes.html', {
+        'lotes': page_obj.object_list,
+        'page_obj': page_obj,
     })
