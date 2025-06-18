12139796bc4a786dcb4151db72be36e7f3c92415
FelipeNascimento-dev <felipe.nasci.dev@gmail.com>
2025-06-18 15:20:37 -0300

    feat: ajustanto permissoes do dash

--- inventario/views/acompanhamento_view.py
+++ inventario/views/acompanhamento_view.py
@@ -1,7 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponseForbidden
 
+@login_required(login_url='inventario:login')
 def acompanhamento_dash(request):
     is_visualizador_master = request.user.groups.filter(name='INV_VISUALIZADOR_MASTER').exists()
+
+    if not is_visualizador_master:
+         return redirect('inventario:index')
+
     return render(request, 'inventario/acompanhamento.html', {
-        'is_visualizador_master': is_visualizador_master, 
+        'is_visualizador_master': is_visualizador_master,
     })
