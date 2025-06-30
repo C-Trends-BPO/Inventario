7325d84c4401a9469914b9985748fb245f3fb05d
FelipeNascimento-dev <felipe.nasci.dev@gmail.com>
2025-06-30 16:51:03 -0300

    feat: troca de ordem formulario de bipagem

--- inventario/forms/bipagem_forms.py
+++ inventario/forms/bipagem_forms.py
@@ -3,6 +3,11 @@ from django import forms
 class BipagemForm(forms.Form):
     nome_formulario = 'Bipagem'
 
+    estado = forms.ChoiceField(
+        required=False,
+        choices=[('', ''), ('GOOD', 'GOOD'), ('BAD', 'BAD'), ('OBSOLETO', 'OBSOLETO'), ('TRIAGEM', 'TRIAGEM')],
+        label='Status'
+    )
     serial = forms.CharField(
     label='Serial',
     max_length=100,
@@ -15,8 +20,3 @@ class BipagemForm(forms.Form):
     )
     modelo = forms.CharField(label='Modelo', max_length=100, required=False)
     patrimonio = forms.CharField(label='Patrimônio', max_length=100, required=False)
-    estado = forms.ChoiceField(
-        required=False,
-        choices=[('', ''), ('GOOD', 'GOOD'), ('BAD', 'BAD'), ('OBSOLETO', 'OBSOLETO'), ('TRIAGEM', 'TRIAGEM')],
-        label='Status'
-    )
