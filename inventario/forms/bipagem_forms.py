57d83822862ff4ff28e6656788c2624838f2de71
FelipeNascimento-dev <felipe.nasci.dev@gmail.com>
2025-06-30 12:12:43 -0300

    feat: ajuste completo no formulario de bipagem

--- inventario/forms/bipagem_forms.py
+++ inventario/forms/bipagem_forms.py
@@ -3,7 +3,16 @@ from django import forms
 class BipagemForm(forms.Form):
     nome_formulario = 'Bipagem'
 
-    serial = forms.CharField(label='Serial', max_length=100)
+    serial = forms.CharField(
+    label='Serial',
+    max_length=100,
+    widget=forms.TextInput(attrs={
+        'id': 'serial-input',
+        'readonly': 'readonly',
+        'onfocus': "this.removeAttribute('readonly');",
+        'autocomplete': 'off'
+    })
+    )
     modelo = forms.CharField(label='Modelo', max_length=100, required=False)
     patrimonio = forms.CharField(label='Patrimônio', max_length=100, required=False)
     estado = forms.ChoiceField(
