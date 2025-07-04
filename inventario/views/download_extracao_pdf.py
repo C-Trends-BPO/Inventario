7f35ade4ac1f3d3a9748e2c1b49db57621d5f815
FelipeNascimento-dev <felipe.nasci.dev@gmail.com>
2025-07-04 10:09:10 -0300

    feat: extração para torre

--- inventario/views/download_extracao_pdf.py
+++ inventario/views/download_extracao_pdf.py
@@ -2,52 +2,36 @@ from django.template.loader import get_template
 from django.http import HttpResponse
 from xhtml2pdf import pisa
 from django.db.models import Count
-from inventario.models import LoteBipagem
+from inventario.models import LoteBipagem, Bipagem
 from datetime import datetime
 from django.contrib.auth.decorators import login_required
 
 @login_required(login_url='inventario:login')
 def download_extracao_pdf(request):
     user = request.user
+    responsavel = user.get_full_name() or user.username
     grupo = user.groups.first()
-    endereco_pa = getattr(grupo.informacoes, "endereco", "Não informado")
-    responsavel = request.user.get_full_name() or request.user.username
+    nome_pa = grupo.name if grupo else "PA Não vinculada"
+    endereco_pa = getattr(grupo.informacoes, "endereco", "Não informado") if grupo else "Não informado"
 
-    if not grupo:
-        return HttpResponse('Usuário sem grupo vinculado.', status=403)
+    lotes = LoteBipagem.objects.filter(user_created=user)
 
-    lotes = (
-        LoteBipagem.objects
-        .filter(group_user=grupo)
-        .annotate(
-            qtd_caixas=Count('caixas', distinct=True),
-            qtd_seriais=Count('caixas__bipagem', distinct=True),
-        )
-        .order_by('numero_lote')
-    )
-
-    resumo_pa = []
-    total_caixas = 0
-    total_seriais = 0
-
-    for lote in lotes:
-        resumo_pa.append({
-            "lote": lote.numero_lote,
-            "caixas": lote.qtd_caixas,
-            "seriais": lote.qtd_seriais,
-        })
-        total_caixas += lote.qtd_caixas
-        total_seriais += lote.qtd_seriais
+    total_lotes = lotes.count()
+    total_caixas = Bipagem.objects.filter(id_caixa__lote__user_created=user).values('id_caixa').distinct().count()
+    total_seriais = Bipagem.objects.filter(id_caixa__lote__user_created=user).count()
 
     context = {
-        "empresa": "C-Trends BPO",
-        "endereco": "R. Bonfim, 81 - Maranhão, São Paulo - SP, 03073-010",
+        "empresa": "Getnet",
         "cnpj": "12.345.678/0001-99",
         "responsavel": responsavel,
-        "nome_pa": grupo.name,
+        "nome_pa": nome_pa,
         "endereco_pa": endereco_pa,
         "data_emissao": datetime.now().strftime('%d/%m/%Y'),
-        "resumo_pa": resumo_pa,
+        "resumo_pa": [{
+            "lote": total_lotes,
+            "caixas": total_caixas,
+            "seriais": total_seriais,
+        }],
         "total_caixas": total_caixas,
         "total_seriais": total_seriais,
     }
