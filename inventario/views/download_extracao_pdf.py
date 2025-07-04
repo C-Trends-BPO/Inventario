b72c5fa8e6d5fedd1cdf0a3368d8f0ec38f81df3
FelipeNascimento-dev <felipe.nasci.dev@gmail.com>
2025-07-04 14:26:43 -0300

    feat: extracao de dados por PA para usuario qualificados

--- inventario/views/download_extracao_pdf.py
+++ inventario/views/download_extracao_pdf.py
@@ -1,3 +1,4 @@
+from django.shortcuts import render
 from django.template.loader import get_template
 from django.http import HttpResponse
 from xhtml2pdf import pisa
@@ -5,7 +6,6 @@ from django.db.models import Count
 from inventario.models import LoteBipagem, Bipagem
 from datetime import datetime
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render
 from django.contrib.auth.models import Group
 
 @login_required(login_url='inventario:login')
@@ -13,17 +13,32 @@ def download_extracao_pdf(request):
     user = request.user
     responsavel = user.get_full_name() or user.username
     grupos = user.groups.values_list('name', flat=True)
+    pa_param = request.GET.get('pa')  # <-- Aqui pegamos a PA selecionada na URL
 
-    is_admin_total = 'INV_PA_GER_TOTAL' in grupos 
+    is_admin_total = 'INV_PA_GER_TOTAL' in grupos
 
-    if is_admin_total:
+    # Caso seja um admin e tenha selecionado uma PA:
+    if is_admin_total and pa_param:
+        lotes = LoteBipagem.objects.filter(group_user__name=pa_param)
+        total_lotes = lotes.count()
+        total_caixas = Bipagem.objects.filter(id_caixa__lote__group_user__name=pa_param).values('id_caixa').distinct().count()
+        total_seriais = Bipagem.objects.filter(id_caixa__lote__group_user__name=pa_param).count()
+
+        nome_pa = pa_param
+        grupo = Group.objects.filter(name=pa_param).first()
+        endereco_pa = getattr(grupo.informacoes, "endereco", "Não informado") if grupo else "Não informado"
+
+    elif is_admin_total and not pa_param:
+        # Acesso geral para todos
         lotes = LoteBipagem.objects.all()
         total_lotes = lotes.count()
         total_caixas = Bipagem.objects.values('id_caixa').distinct().count()
         total_seriais = Bipagem.objects.count()
         nome_pa = "TODAS AS PAs"
         endereco_pa = "Consolidado Geral"
+
     else:
+        # Usuários normais
         grupo = user.groups.first()
         nome_pa = grupo.name if grupo else "PA Não vinculada"
         endereco_pa = getattr(grupo.informacoes, "endereco", "Não informado") if grupo else "Não informado"
@@ -60,7 +75,38 @@ def download_extracao_pdf(request):
         return HttpResponse('Erro ao gerar PDF', status=500)
     return response
 
+
 @login_required(login_url='inventario:login')
 def relatorios_view(request):
-    grupos = Group.objects.filter(name__startswith='INV_PA_')
-    return render(request, 'inventario/relatorios.html', {'grupos': grupos})
+    user = request.user
+    username = user.username.lower()
+
+    usuarios_admins = ['adm_tecnico', 'adm_gtn', 'adm_auditoria']
+    is_admin = username in usuarios_admins
+
+    if is_admin:
+        grupos = Group.objects.exclude(name__in=["INV_PA_GER_TOTAL", "INV_PA_VISUALIZADOR_MASTER"])
+    else:
+        grupos = user.groups.exclude(name__in=["INV_PA_GER_TOTAL", "INV_PA_VISUALIZADOR_MASTER"])
+
+    pa_selecionada = request.GET.get('pa')
+    dados_pa = []
+
+    if pa_selecionada:
+        lotes = LoteBipagem.objects.filter(group_user__name=pa_selecionada)
+        for lote in lotes:
+            total_seriais = Bipagem.objects.filter(id_caixa__lote=lote).count()
+            total_caixas = lote.caixas.count()
+            dados_pa.append({
+                'lote': lote.numero_lote,
+                'status': lote.status,
+                'criado_por': lote.user_created.username,
+                'total_caixas': total_caixas,
+                'total_seriais': total_seriais
+            })
+
+    return render(request, 'inventario/relatorios.html', {
+        'grupos': grupos,
+        'dados_pa': dados_pa,
+        'pa_selecionada': pa_selecionada,
+    })
