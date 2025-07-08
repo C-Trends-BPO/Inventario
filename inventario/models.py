6ccd475e68b320cc301313bea32354416f47a309
FelipeNascimento-dev <felipe.nasci.dev@gmail.com>
2025-07-08 09:21:32 -0300

    feat: ajuste na edicao ou exclusão

--- inventario/models.py
+++ inventario/models.py
@@ -1,5 +1,5 @@
-from django.db import models
-from django.contrib.auth.models import Group
+from django.db import models # type: ignore
+from django.contrib.auth.models import Group # type: ignore
 from decimal import Decimal
 
 class LoteBipagem(models.Model):
