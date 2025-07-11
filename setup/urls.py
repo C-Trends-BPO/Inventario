f0f28e160efd3aa9b66f337e3f8232978b5c58ac
FelipeNascimento-dev <felipe.nasci.dev@gmail.com>
2025-07-11 10:54:46 -0300

    feat: url personalizada

--- setup/urls.py
+++ setup/urls.py
@@ -1,7 +1,9 @@
+from django.shortcuts import redirect
 from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path('', lambda request: redirect('inventario/', permanent=False)),
     path('inventario/', include('inventario.urls')),
     path('inventario/admin/', admin.site.urls),
 ]
